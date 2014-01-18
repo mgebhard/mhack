dec0e0f52012c8a20e641f1a5b82bc6f4d438ab6
Megan Gebhard <megangebhard@Megans-MacBook-Pro-2.local>
2014-01-18 15:36:43 -0500

    working main

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from google.appengine.api import users
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class User(ndb.Model):
+class Account(ndb.Model):
     user = ndb.UserProperty(required=True)
     points = ndb.IntegerProperty(required=False)
 
 class ImageEvent(ndb.Model):
     src = ndb.StringProperty(required=True)
-    sender = ndb.KeyProperty(User)
-    receiver = ndb.KeyProperty(User)
+    sender = ndb.KeyProperty(Account)
+    receiver = ndb.KeyProperty(Account)
     answer = ndb.StringProperty(required=True)
 
 def RenderTemplate(template_name, values):
@@ -24,7 +24,7 @@ def RenderTemplate(template_name, values):
     return template.render(values)
 
 def getUser(usr):
-    return User.query().filter(User.user==usr).get()
+    return Account.query().filter(Account.user==usr).get()
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
@@ -34,14 +34,14 @@ class HomeHandler(webapp2.RequestHandler):
 
         # Create new user if not found in the db
         if not userData:
-            new_user = User(user=current_user, 
-                            points=0)
+            new_user = Account(user=current_user, 
+                                points=0)
             new_user.put()
             self.response.out.write(RenderTemplate('home.html', {'photoList': []}))
 
         # Fetch photos
         photo_objects = []
-        new_photos = ImageEvent.query().filter(ImageEvent.receiver==userData.key).get()
+        new_photos = ImageEvent.query().filter(ImageEvent.receiver==userData.key)
         if new_photos:
             for photo_instance in new_photos:
                 photo_objects.append(photo_instance)
@@ -59,8 +59,14 @@ class SendHandler(webapp2.RequestHandler):
         src = self.request.get('pic_src')
         userData = getUser(current_user)
         friend = users.User('mgebhard1995@gmail.com')
+        friendData = getUser(friend)
+        if not friendData:
+            friendData = Account(user=current_user, 
+                                points=0)
+            friendData.put()
+
         pic_event = ImageEvent(sender=userData.key, 
-                               receiver=friend, 
+                               receiver=friendData.key, 
                                src=src,
                                answer='cat')
         pic_event.put()
