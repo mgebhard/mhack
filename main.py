23376355fe329f5225219ec5a454efcdad6a15da
Megan Gebhard <megangebhard@Megans-MacBook-Pro-2.local>
2014-01-18 15:13:50 -0500

    main debug

--- main.py
+++ main.py
@@ -11,56 +11,55 @@ jinja_environment = jinja2.Environment(loader=
 
 class User(ndb.Model):
     user = ndb.UserProperty(required=True)
-    points = ndb.IntegerProprty(required=False)
+    points = ndb.IntegerProperty(required=False)
 
 class ImageEvent(ndb.Model):
     src = ndb.StringProperty(required=True)
-    sender = ndb.ReferenceProperty(User)
-    receiver = ndb.ReferenceProperty(User)
+    sender = ndb.KeyProperty(User)
+    receiver = ndb.KeyProperty(User)
     answer = ndb.StringProperty(required=True)
 
 def RenderTemplate(template_name, values):
     template = jinja_environment.get_template(template_name)
     return template.render(values)
 
-def getUser():
-    current_user = users.get_current_user()
-    return User.query().filter(User.user==current_user).get()
+def getUser(usr):
+    return User.query().filter(User.user==usr).get()
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
         # check to see if the user is a new user
-        userData = getUser()
+        current_user = users.get_current_user()
+        userData = getUser(current_user)
 
         # Create new user if not found in the db
         if not userData:
             new_user = User(user=current_user, 
                             points=0)
             new_user.put()
-            userData = new_user
+            self.response.out.write(RenderTemplate('home.html', {'photoList': []}))
 
         # Fetch photos
         photo_objects = []
-        new_photos = ImageEvent.query().filter(ImageEvent.receiver==userData.key()).get()
-        for photo_instance in new_photos:
-            photo_objects.append(photo_instance)
+        new_photos = ImageEvent.query().filter(ImageEvent.receiver==userData.key).get()
+        if new_photos:
+            for photo_instance in new_photos:
+                photo_objects.append(photo_instance)
 
         self.response.out.write(RenderTemplate('home.html', {'photoList': photo_objects}))
 
 
-
-
 class SendHandler(webapp2.RequestHandler):
     def get(self):
-        usr = users.get_current_user()
-        self.response.out.write(RenderTemplate('home.html', {}))
+        self.response.out.write(RenderTemplate('send.html', {}))
 
 
     def post(self):
+        current_user = users.get_current_user()
         src = self.request.get('pic_src')
-        userData = getUser()
+        userData = getUser(current_user)
         friend = users.User('mgebhard1995@gmail.com')
-        pic_event = ImageEvent(sender=userData.key(), 
+        pic_event = ImageEvent(sender=userData.key, 
                                receiver=friend, 
                                src=src,
                                answer='cat')
